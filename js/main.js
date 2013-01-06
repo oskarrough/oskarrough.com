22e6304fac879b1954963db32c31fba434f2c519
Oskar Rough <oskarrough@gmail.com>
2013-01-06 23:22:46 +0100

    Even more updates

--- js/main.js
+++ js/main.js
@@ -4,37 +4,30 @@ $(function(){
 	if (monster.get('theme')) {
 		$('body').removeClass('cosmic proinquotes funky').addClass(monster.get('theme'));
 	}
-	$('.sister-sledge').addClass('visible');
+
+	//$('.sister-sledge').addClass('visible');
 
 	// Theme chooser
 	$('#cosmic, #proinquotes, #funky').click(function(){
-
 		if (monster.get('theme')) {
 			monster.remove('theme');
 		}
 		var theme = $(this).attr('id');
 		monster.set('theme', theme, 1);
 		$('body').removeClass('cosmic proinquotes funky').addClass(monster.get('theme'));
-
+		console.log(monster.get('theme'));
 	});
 
-
-	// Cosmic effect
-	// document.addEventListener('touchstart', function(e) {
-	//     e.preventDefault();
-	//     var touch = e.touches[0];
-	//     alert(touch.pageX + " - " + touch.pageY);
-	// }, false);
 	function justdoit(){
 		$('body').removeClass('imsomoved');
 	}
 	$(".cosmic").mousemove(function(e){
 		$(this).addClass('imsomoved');
-		setTimeout(justdoit, 10000)
+		setTimeout(justdoit, 10000);
 		// var pageCoords = "( " + e.pageX + ", " + e.pageY + " )";
 		// var clientCoords = "( " + e.clientX + ", " + e.clientY + " )";
-		// $("span:first").text("( e.pageX, e.pageY ) : " + pageCoords);
-		// $("span:last").text("( e.clientX, e.clientY ) : " + clientCoords);
+		// console.log("( e.pageX, e.pageY ) : " + pageCoords);
+		// console.log("( e.clientX, e.clientY ) : " + clientCoords);
 	});
 
 
