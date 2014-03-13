be8138899132f8937387f769e9aaf217a240ad86
Oskar Rough <oskarrough@gmail.com>
2014-03-13 21:45:19 +0100

    Remove unused components

--- components/rem/test/config.rb
+++
@@ -1,7 +0,0 @@
-# Compass config file
-# To install compass gem install compass (you may be required to use the 'sudo' command to install gems)
-
-http_path       = "/"
-css_dir         = "css"
-sass_dir        = "sass"
-line_comments   = false
