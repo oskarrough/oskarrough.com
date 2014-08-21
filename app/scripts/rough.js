80d59917c516d2d54db768a151e66c3227f5baa3
Oskar Rough <oskarrough@gmail.com>
2014-08-22 01:20:07 +0200

    Clean and update styles and add themes

--- app/scripts/rough.js
+++ app/scripts/rough.js
@@ -1,11 +1,38 @@
+$.pjax.defaults.timeout = 5000
+
 /*global Blazy*/
 var Rough = function() {
+	this.$navLinks = $('.Nav').find('a');
 	this.init();
 };
 
 Rough.prototype = {
 	init: function() {
+		console.log('hej');
 		// this.lazyLoading();
+
+		$(document).pjax('a', '#Main');
+		$(document).on('pjax:send', function() {
+		  console.log('pjax:send');
+		});
+
+		var d = new Date();
+		var n = d.getTime();
+		// $('body').prepend('<time>'+ n +'</time>');
+
+		this.$navLinks.on('click', this.load.bind(this));
+	},
+
+	load: function(event) {
+		event.preventDefault();
+		// update nav
+		this.$navLinks.removeClass('is-active');
+		var $el = $(event.currentTarget).addClass('is-active');
+		// load and insert page with ajax
+		var url = $el.attr('href');
+		var stateObj = { foo: "bar" };
+		$('#Main').load(url + ' #Main');
+		history.pushState(stateObj, "what is this", url);
 	},
 
 	/**
