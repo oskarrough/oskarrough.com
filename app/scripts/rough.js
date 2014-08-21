e0d645eeb3a2f42e6196252fdbb4c6a8c2235e67
Oskar Rough <oskarrough@gmail.com>
2014-08-21 23:56:56 +0200

    Fix bower scripts

--- app/scripts/rough.js
+++ app/scripts/rough.js
@@ -18,5 +18,40 @@ Rough.prototype = {
 			selector: '.Gif',
 			offset: 100
 		});
+	},
+
+	themes: function() {
+		// If a theme is chosen
+		if (monster.get('theme')) {
+			$('body').removeClass('cosmic proinquotes funky').addClass(monster.get('theme'));
+		}
+
+		//$('.sister-sledge').addClass('visible');
+
+		// Theme chooser
+		$('#cosmic, #proinquotes, #funky').click(function(){
+			if (monster.get('theme')) {
+				monster.remove('theme');
+			}
+			var theme = $(this).attr('id');
+			monster.set('theme', theme, 1);
+			$('body').removeClass('cosmic proinquotes funky').addClass(monster.get('theme'));
+			console.log(monster.get('theme'));
+		});
+	},
+
+	justdoit: function() {
+		$('body').removeClass('imsomoved');
+	},
+
+	actions: function() {
+		$('.cosmic').mousemove(function(e){
+			$(this).addClass('imsomoved');
+			setTimeout(justdoit, 10000);
+			// var pageCoords = "( " + e.pageX + ", " + e.pageY + " )";
+			// var clientCoords = "( " + e.clientX + ", " + e.clientY + " )";
+			// console.log("( e.pageX, e.pageY ) : " + pageCoords);
+			// console.log("( e.clientX, e.clientY ) : " + clientCoords);
+		});
 	}
 };
