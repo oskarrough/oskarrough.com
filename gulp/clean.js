880033999db660f260b17a5ec498a4638a11243f
Oskar Rough <oskarrough@gmail.com>
2015-09-06 19:35:15 +0200

    Update packages and del task

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,4 +1,11 @@
 const gulp = require('gulp');
 const del = require('del');
 
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+gulp.task('clean', () => {
+	return del([
+		'.tmp',
+		'dist'
+	]);
+});
+
+// If you want to exclude files remember https://github.com/sindresorhus/del#beware
