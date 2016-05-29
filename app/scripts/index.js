0e59f2f5ac60c1193494efb060cb9e89ccc820e0
Oskar Rough <oskarrough@gmail.com>
2016-05-29 22:23:05 +0200

    Update packages, lint and fix https

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,10 +1,12 @@
-const starwars = require('./starwars')
 const FontFaceObserver = require('fontfaceobserver')
+const starwars = require('./starwars')
 
-const font = new FontFaceObserver('triplicate-t4', {/*weight: 400*/})
+const font = new FontFaceObserver('triplicate-t4', {
+	/* weight: 400*/
+})
 font.load().then(() => {
 	document.documentElement.classList.remove('no-fonts')
-});
+})
 
 window.onload = () => {
 	starwars.init('#starwars')
--- test.js
+++ test.js
@@ -1,33 +1,31 @@
-import test from 'ava';
-
-// Special modules needed for these tests.
-import fs from 'fs';
-import gulp from 'gulp';
-import glob from 'glob';
-require('./gulpfile');
+import fs from 'fs'
+import gulp from 'gulp'
+import test from 'ava'
+import glob from 'glob'
+require('./gulpfile')
 
 test('we have the required structure', t => {
-	t.true(fs.lstatSync('app').isDirectory());
-	t.true(fs.lstatSync('gulp').isDirectory());
-	t.true(fs.lstatSync('gulpfile.js').isFile());
-	t.true(fs.lstatSync('package.json').isFile());
-});
+	t.true(fs.lstatSync('app').isDirectory())
+	t.true(fs.lstatSync('gulp').isDirectory())
+	t.true(fs.lstatSync('gulpfile.js').isFile())
+	t.true(fs.lstatSync('package.json').isFile())
+})
 
 test('we have gulp tasks', t => {
-	const tasks = ['build', 'browserify', 'clean', 'default', 'handlebars', 'sass', 'scripts', 'serve', 'styles', 'templates'];
-	t.plan(tasks.length);
+	const tasks = ['build', 'browserify', 'clean', 'default', 'handlebars', 'sass', 'scripts', 'serve', 'styles', 'templates']
+	t.plan(tasks.length)
 	for (let i = 0; i < tasks.length; i++) {
-		t.ok(gulp.tasks[tasks[i]]);
+		t.truthy(gulp.tasks[tasks[i]])
 	}
-});
+})
 
 // Expects that you've run `npm run build` earlier.
 test('it builds and compiles', async t => {
-	t.plan(3);
-	const templates = await glob.sync('dist/*.html');
-	t.ok(templates.length);
-	const styles = await glob.sync('dist/styles/*.css');
-	t.ok(styles.length);
-	const scripts = await glob.sync('dist/scripts/*.js');
-	t.ok(scripts.length);
-});
+	t.plan(3)
+	const templates = await glob.sync('dist/*.html')
+	t.truthy(templates.length)
+	const styles = await glob.sync('dist/styles/*.css')
+	t.truthy(styles.length)
+	const scripts = await glob.sync('dist/scripts/*.js')
+	t.truthy(scripts.length)
+})
